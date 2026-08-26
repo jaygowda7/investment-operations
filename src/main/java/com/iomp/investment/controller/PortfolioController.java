726220c7022f5b28d0467272cc99c55e89dcb5c7
JAYANTH GOWDA C <jayanthgowda17718@gmail.com>
2026-08-27 00:14:03 +0530

    Add portfolio service layer

--- src/main/java/com/iomp/investment/controller/PortfolioController.java
+++ src/main/java/com/iomp/investment/controller/PortfolioController.java
@@ -4,12 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.iomp.investment.service.PortfolioService;
+
 @RestController
 @RequestMapping("/api/portfolios")
 public class PortfolioController {
 	
+	private final PortfolioService portfolioService;
+	
+	public PortfolioController(PortfolioService portfolioService) {
+		this.portfolioService=portfolioService;
+	}
+
+	
 	@GetMapping("/hello")
     public String hello() {
-        return "Investment Operations Platform is running";
+        return portfolioService.getMessage();
     }
 }
---
+++ src/main/java/com/iomp/investment/service/PortfolioService.java
@@ -0,0 +1,12 @@
+package com.iomp.investment.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PortfolioService {
+
+		public String getMessage() {
+			return "Investment Application Running via Service Layer";
+		}
+
+}
